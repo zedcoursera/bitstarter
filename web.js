f0d5a8e92c7941ebf00af69179a77935ddd73765
zedcoursera <zed.coursera@citromail.hu>
2013-07-06 13:19:48 +0200

    using index.html

--- web.js
+++ web.js
@@ -1,9 +1,11 @@
 var express = require('express');
+var fs      = require('fs');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+  var hello = fs.readFileSync("index.html");
+  response.send(hello.toString());
 });
 
 var port = process.env.PORT || 5000;
